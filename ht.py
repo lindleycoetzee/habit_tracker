420611d7747c59d4be053f6df761e4cba8ff5b19
Lindley Coetzee <86111506+lindleycoetzee@users.noreply.github.com>
2024-04-24 11:09:30 +0200

    Update ht.py
    Update before going live

--- ht.py
+++ ht.py
@@ -35,7 +35,8 @@ table = dag.AgGrid(
     columnDefs=[{"field": c} for c in df.columns],
     rowData=df.to_dicts(),
     columnSize="sizeToFit",
-    style = {'backgroundColor': '#0074D9', "color": "white","height": 340},
+    style={"backgroundColor": "#0074D9", "color": "white", "height": 270},
+    dashGridOptions={"rowHeight": 30},
 )
 
 ##assign values
@@ -65,147 +66,163 @@ fig_list = []
 for c in df.columns[1:]:
     fig_c = go.Figure()
     fig_c.add_trace(go.Bar(x=df["date"], y=df[c]))
-    fig_c.update_traces(marker_color="#335eff")
-    fig_c.update_layout(height = 300)
+    fig_c.update_layout(height=290)
+    if c == "pages read":
+        fig_c.update_traces(marker_color="#335eff")
+        fig_c.update_layout(
+            title=f"Read {pages_read} pages in {pages_read_c} days at {round(pages_read/pages_read_c,2)} pages per day"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "code":
+        fig_c.update_traces(marker_color="#335eff")
+        fig_c.update_layout(
+            title=f"Coded {days_coding} out of {days_coding_c} days = {round(days_coding/days_coding_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "blog":
+        fig_c.update_traces(marker_color="#335eff")
+        fig_c.update_layout(
+            title=f"Blogged {days_blogging} out of {days_blogging_c} days = {round(days_blogging/days_blogging_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "meditation":
+        fig_c.update_traces(marker_color="#335eff")
+        fig_c.update_layout(
+            title=f"Meditated {days_meditated} out of {days_meditated_c} days = {round(days_meditated/days_meditated_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "journal entry":
+        fig_c.update_traces(marker_color="#335eff")
+        fig_c.update_layout(
+            title=f"{jounal_entries} Jounal Entries out of {jounal_entries_c} days = {round(jounal_entries/jounal_entries_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "min excercise ":
+        fig_c.update_traces(marker_color="#ff5733")
+        fig_c.update_layout(
+            title=f"{minutes_exercised} Minutes exercised in {minutes_exercised_c} days at {round(minutes_exercised/minutes_exercised_c,2)} minutes per day"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "in bed at 22:00":
+        fig_c.update_traces(marker_color="#ff5733")
+        fig_c.update_layout(
+            title=f"Days slept before 22h00 : {days_slept_before_22h00} = {round(days_slept_before_22h00/days_slept_before_22h00_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "wake up at 05h00":
+        fig_c.update_traces(marker_color="#ff5733")
+        fig_c.update_layout(
+            title=f"Days woke up at 05h00 : {days_woke_up_05h00} = {round(days_woke_up_05h00/days_woke_up_05h00_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "alcohol":
+        fig_c.update_traces(marker_color="#1fb81d")
+        fig_c.update_layout(
+            title=f"Days without alcohol : {days_alcohol_free} = {round(days_alcohol_free/jounal_entries_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "meat":
+        fig_c.update_traces(marker_color="#1fb81d")
+        fig_c.update_layout(
+            title=f"Days without meat : {days_without_meat} = {round(days_without_meat/jounal_entries_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
+    if c == "sugar":
+        fig_c.update_traces(marker_color="#1fb81d")
+        fig_c.update_layout(
+            title=f"Days without sugar : {days_without_sugar} = {round(days_without_sugar/jounal_entries_c * 100,2)}%"
+        )
+        fig_c.update_layout(
+            title=dict(
+                x=0.5,
+                y=0.95,
+                font=dict(family="Cabin Sketch, cursive", size=20, color="#000000"),
+            ),
+            font=dict(family="Comic Sans MS", size=12),
+        )
     fig_list.append(fig_c)
 
-ex_fig = fig_list[5].update_traces(marker_color="#ff5733")
-sl_fig = fig_list[6].update_traces(marker_color="#ff5733")
-wk_fig = fig_list[7].update_traces(marker_color="#ff5733")
-
-al_fig = fig_list[8].update_traces(marker_color="#1fb81d")
-mt_fig = fig_list[9].update_traces(marker_color="#1fb81d")
-sg_fig = fig_list[10].update_traces(marker_color="#1fb81d")
-
-tab_pages = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(html.H3(f"Read {pages_read} pages in {pages_read_c} days at {round(pages_read/pages_read_c,2)} pages per day")),
-                dcc.Graph(figure=fig_list[0]),
-            ]
-        ),
-    ]
-)
-tab_code = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(
-                    html.H3(f"Coded {days_coding} out of {days_coding_c} days = {round(days_coding/days_coding_c * 100,2)}%")
-                ),
-                dcc.Graph(figure=fig_list[1]),
-            ]
-        ),
-    ]
-)
-tab_blog = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(
-                    html.H3(f"Blogged {days_blogging} out of {days_blogging_c} days = {round(days_blogging/days_blogging_c * 100,2)}%")
-                ),
-                dcc.Graph(figure=fig_list[2]),
-            ]
-        ),
-    ]
-)
-tab_med = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(
-                    html.H3(
-                        f"Meditated {days_meditated} out of {days_meditated_c} days = {round(days_meditated/days_meditated_c * 100,2)}%"
-                    )
-                ),
-                dcc.Graph(figure=fig_list[3]),
-            ]
-        ),
-    ]
-)
-tab_jnl = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(
-                    html.H3(
-                        f"{jounal_entries} Jounal Entries out of {jounal_entries_c} days = {round(jounal_entries/jounal_entries_c * 100,2)}%"
-                    )
-                ),
-                dcc.Graph(figure=fig_list[4]),
-            ]
-        ),
-    ]
-)
-tab_ex = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(
-                    html.H3(
-                        f"{minutes_exercised} Minutes exercised in {minutes_exercised_c} days at {round(minutes_exercised/minutes_exercised_c,2)} minutes per day"
-                    )
-                ),
-                dcc.Graph(figure=ex_fig),
-            ]
-        ),
-    ]
-)
-tab_slp = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(
-                    html.H3(f"Days slept before 22h00: {days_slept_before_22h00} = {round(days_slept_before_22h00/days_slept_before_22h00_c * 100,2)}%")
-                ),
-                dcc.Graph(figure=fig_list[6]),
-            ]
-        ),
-    ]
-)
-tab_wake = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(html.H3(f"Days woke up at 05h00 : {days_woke_up_05h00} = {round(days_woke_up_05h00/days_woke_up_05h00_c * 100,2)}%")),
-                dcc.Graph(figure=fig_list[7]),
-            ]
-        ),
-    ]
-)
-tab_alc = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(html.H3(f"Alcohol free days : {days_alcohol_free} = {round(days_alcohol_free/jounal_entries_c * 100,2)}%")),
-                dcc.Graph(figure=fig_list[8]),
-            ]
-        ),
-    ]
-)
-tab_meat = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(html.H3(f"Days without meat : {days_without_meat} = {round(days_without_meat/jounal_entries_c * 100,2)}%")),
-                dcc.Graph(figure=fig_list[9]),
-            ]
-        ),
-    ]
-)
-tab_sug = dbc.Card(
-    [
-        dbc.CardBody(
-            [
-                html.Center(html.H3(f"Days without sugar : {days_without_sugar} = {round(days_without_sugar/jounal_entries_c * 100,2)}%")),
-                dcc.Graph(figure=fig_list[10]),
-            ]
-        ),
-    ]
-)
-
+tab_pages = dcc.Graph(figure=fig_list[0])
+tab_code = dcc.Graph(figure=fig_list[1])
+tab_blog = dcc.Graph(figure=fig_list[2])
+tab_med = dcc.Graph(figure=fig_list[3])
+tab_jnl = dcc.Graph(figure=fig_list[4])
+tab_ex = dcc.Graph(figure=fig_list[5])
+tab_slp = dcc.Graph(figure=fig_list[6])
+tab_wake = dcc.Graph(figure=fig_list[7])
+tab_alc = dcc.Graph(figure=fig_list[8])
+tab_meat = dcc.Graph(figure=fig_list[9])
+tab_sug = dcc.Graph(figure=fig_list[10])
 
 tabs = dbc.Container(
     [
@@ -233,7 +250,6 @@ def create_ht():
         [
             dbc.Row(
                 [
-                    html.Br(),
                     html.Center(html.H1("Live Habit Tracker")),
                 ]
             ),
@@ -247,10 +263,7 @@ def create_ht():
                     table,
                 ]
             ),
-        ],style={"overflow-x": "hidden"}
+        ],
+        style={"overflow-x": "hidden"},
     )
     return layout
-
-####run app
-##if __name__ == "__main__":
-##    app.run(debug=True)
